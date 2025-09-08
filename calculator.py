ceb49abdcc7ada194e1f817cbefcfef2d60e5d94
rayonayoung <rayspost@gmail.com>
2025-09-08 14:14:52 -0700

    Add message prompts to json file

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
+import json
+
+with open('calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
+
 def prompt(message):
     print(f"==> {message}")
 
@@ -11,29 +16,29 @@ def invalid_number(number_str):
 
 continue_message = 'yes'
 
-prompt('Welcome to Calculator!')
+prompt(MESSAGES["welcome"])
 
 while continue_message == 'yes':
 
-    prompt("What's the first number?")
+    prompt(MESSAGES["first_number"])
     number1 = input()
 
     while invalid_number(number1):
-        prompt("Hmm... that doesn't look like a valid number.")
+        prompt(MESSAGES["invalid_number"])
         number1 = input()
 
-    prompt("What's the second number?")
+    prompt(MESSAGES["second_number"])
     number2 = input()
 
     while invalid_number(number2):
-        prompt("Hmm... that doesn't look like a valid number.")
+        prompt(MESSAGES["invalid_number"])
         number2 = input()
 
-    prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide")
+    prompt(MESSAGES["operation"])
     operation = input()
 
     while operation not in ["1", "2", "3", "4"]:
-        prompt("You must choose 1, 2, 3, or 4")
+        prompt(MESSAGES["invalid_operation"])
         operation = input()
 
     match operation:
@@ -46,6 +51,6 @@ while continue_message == 'yes':
         case "4":
             output = int(number1) / int(number2)
 
-    prompt(f"The result is {output}")
-    prompt(f'Perform another calculation?')
+    prompt(f'{MESSAGES["result"]}{output}')
+    prompt(MESSAGES["continue"])
     continue_message = input()
